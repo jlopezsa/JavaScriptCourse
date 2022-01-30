4ca3983f938a5a06fa7251b7196c43df40b00f39
JulianLopez <jlopezsa@gmail.com>
2022-01-29 23:35:13 -0500

    Message is shown in browser

--- notesApp/index.js
+++ notesApp/index.js
@@ -3,11 +3,17 @@ const app = express();
 
 // (property) Application<Record<string, any>>.get: ((name: string) => any) & IRouterMatcher<Express, any>
 app.get("/",(req,res)=>{
-    res.send("<h1>Hola Julian Lopez</h1>")
+    res.send("<h1>Hola Julian Jair Salamanca</h1>")
 });
+
+// Creating routes (Routing in Express): the routs 
+app.post("/users",(req,res)=>{
+    // HTTP states codes: https://es.wikipedia.org/wiki/Anexo:C%C3%B3digos_de_estado_HTTP
+    //res.status(404).send("No se encontró el recurso");
+    res.sendStatus(404).send("Not found");
+})
 
 // Turn on the server
 // (method) Application<Record<string, any>>.listen(port: number, hostname: string, backlog: number, callback?: () => void): Server (+5 overloads)
-
 app.listen(3000,()=>console.log("Listening on port 3000 ... "));
 
