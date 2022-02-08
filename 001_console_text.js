cc73f7526b1c5bb5cb0b46f435ae2330a78942a2
Julian Lopez <jlopezsa@gmail.com>
2022-02-07 20:31:22 -0500

    Show many forms to print a message in console

---
+++ 001_console_text.js
@@ -0,0 +1,10 @@
+/** JavaScript fundamentals
+ * 1st version: 07/02/2022
+ * Author: Julián López
+ * Aim: Four forms to print a message in terminal 
+ */
+
+console.log("First program in JavaScript, message with \"...\"");
+console.log('Another line code with simple \'...\'');
+console.log('Message '+'with '+'concatenation '+'symbol, '+ '+');
+console.log(`Message with \`...\``);
