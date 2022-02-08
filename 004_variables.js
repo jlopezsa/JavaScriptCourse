a4cea31db43345ccdba856fd75b3885c3cc87234
Julian Lopez <jlopezsa@gmail.com>
2022-02-07 21:09:52 -0500

    Show variables and print values in console

---
+++ 004_variables.js
@@ -0,0 +1,15 @@
+/** JavaScript fundamentals
+ * 1st version: 07/02/2022
+ * Author: Julián López
+ * Aim: Variables
+ */
+
+var side_square = 4.5;
+console.log(`Saquare area (side \= ${4.5}) is: ${side_square*side_square}`);
+
+var unsig_value;
+console.log(`Variable without value is: ${unsig_value}`);
+
+var apellido = `López`;
+
+console.log(`Author of this code is Julián ${apellido}`);
