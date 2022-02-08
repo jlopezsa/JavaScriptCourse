4a38161bfb81e6795b91e8387eca301f8eaf4602
Julian Lopez <jlopezsa@gmail.com>
2022-02-07 20:56:44 -0500

    Logical operators

---
+++ 003_loical_operators.js
@@ -0,0 +1,18 @@
+/** JavaScript fundamentals
+ * 1st version: 07/02/2022
+ * Author: Julián López
+ * Aim: Test logical operators
+ */
+
+console.log(`1===1 \t\t \-\> ${1===1}`);       // true
+console.log(`1===\"1\" \t \-\> ${1==="1"}`); // false
+console.log(`3\<2 \t\t \-\> ${3<2}`);          // false
+console.log(`2\>0 \t\t \-\> ${2>0}`);          // true
+console.log(`7\>\= \t\t \-\> ${7>=7}`);        // true 
+console.log(`6\<\=5 \t\t \-\> ${6<=5}`);       // false
+console.log(`3 \!\=\= 3 \t \-\> ${3!==3}`);     // false
+console.log(`\"hola\" !== \"hola\"\-\> ${"hola"!=="hola"}`); // false
+console.log(`4 \!\=\= "4" \t \-\> ${4 !== "4"}`);    // true
+console.log(`false \=\=\= true \t \-\> ${false === true}`);  // false
+console.log(`true \t\t \-\> ${true}`);         // true
+console.log(`false \t\t \-\> ${false}`);       // false
