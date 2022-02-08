45c431ea6f381a9828f78fa300420fb6714955d7
Julian Lopez <jlopezsa@gmail.com>
2022-02-07 20:41:49 -0500

    Print numbers in console

---
+++ 002_number_console.js
@@ -0,0 +1,9 @@
+/** JavaScript fundamentals
+ * 1st version: 07/02/2022
+ * Author: Julián López
+ * Aim: Print numbers in console
+ */
+
+ console.log("Message to print 2 + 8 = ", 2+8 );
+ console.log('Message to print' + 12.5 +'/'+ '3' + ' = '+ 12.5/3 + ', using \'...\'');
+ console.log(`Message to print ${88} + ${122} = ${88 + 122}, using the expression \$\{...\}`);
