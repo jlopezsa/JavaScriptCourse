5ab6ecbc577fc5c5f847627b158d9f2df9299630
Julian Lopez <jlopezsa@gmail.com>
2022-02-08 08:49:53 -0500

    Add function to compare an character in a string

--- 009_strings.js
+++ 009_strings.js
@@ -3,3 +3,27 @@
  * Author: Julián López
  * Aim: Strings
  */
+
+/** Objetivo: Escribir una función llamada countingEs que reciba como parámetro un string (cadena de texto) y retorne el número de caracters "e" (minúscula).
+ * console.log(countingEs("Estoy ejercitándome los miércoles y jueves") === 6);  // true
+ * console.log(countingEs("Eeeeh!, que alegría verlos amigos mios")  === 6); // true
+ * console.log(countingEs("Ejemplares son las personas de este hermoso pueblo")  === 8); // true
+ * console.log(countingEs("Ahora qué?") === 0); // true
+ * console.log(countingEs("Los elefantes son enormes") === 5) // true
+*/
+
+const str = "Los elefantes son enormes";
+function countingEs(str){
+    var counter = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === 'e') {
+            counter = counter + 1;
+        }
+        
+    }
+    return counter;
+}
+
+console.log(countingEs(str));
+console.log(countingEs(str) === 5);
+
